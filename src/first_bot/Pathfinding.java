cdc07753f528ddfe6cdb517a71dcfe420c2b7d42
Matthew Ho <homattieho@gmail.com>
2022-01-04 11:24:46 -0800

    Fixed a bug in bellman-ford

--- src/first_bot/Pathfinding.java
+++ src/first_bot/Pathfinding.java
@@ -15,6 +15,7 @@ public class Pathfinding {
 
     static int[5][5] rubbleLevels;
     static int[5][5] distances;
+    //will store the direction used to get to a particular square in bellman-ford
     static int[5][5] predecessor; 
 
     public Pathfinding(RobotCommon robot){
@@ -30,7 +31,7 @@ public class Pathfinding {
                 rubbleLevels[dx][dy] = RobotController.senseRubble(MapLocation.add(MapLocation(dx - 2, dy - 2), currentLocation));
                 //if the spot is occupied, pretend like the rubble level is just a really large number
                 if (RobotController.canSenseRobotAtLocation(MapLocation.add(MapLocation(dx - 2, dy - 2), currentLocation)){
-                    rubbleLevels[dx][dy] = 100000;
+                    rubbleLevels[dx][dy] = 10000;
                 }
                 distances[dx][dy] = 1000000;
             }
@@ -43,7 +44,12 @@ public class Pathfinding {
 
     //http://web.mit.edu/agrebe/www/battlecode/20/index.html#navigation see navigation section
     //also not 100% sure the iteration order is best here (going from one corner to the other in bellman-ford)
-    public static void bellmanFord(MapLocation destination){
+    public void bellmanFord(MapLocation destination){
+        // MapLocation intermediateDestination = currentLocation;
+        // //so our destination is actually within range lol
+        // if (abs(currentLocation.x - destination.x) <= 2 && abs(currentLocation.y - destination.y) <= 2){
+        //     intermediateDestination = destination;
+        // }
         //iters should probably be 1 or 2 because greediness is good
         for (iters = 2; iters >= 0; iters--){
             for (int dx = 4; dx >= 0; dx--){
@@ -51,47 +57,30 @@ public class Pathfinding {
                     for (int directionIdx = 7; directionIdx >= 0; directionIdx--){
                         if (dx + dxDiff[directionIdx] >= 0 && dx + dxDiff[directionIdx] <= 4 && 
                             dy + dyDiff[directionIdx] >= 0 && dy + dyDiff[directionIdx] <= 4 && ){
-                            if (distances[dx][dy] + rubbleLevels[dx][dy] < distances[dx + dxDiff[directionIdx]][dy + dyDiff[directionIdx]]){
+                            if (distances[dx][dy] + rubbleLevels[dx + dxDiff[directionIdx][dy + dyDiff[directionIdx]] < distances[dx + dxDiff[directionIdx]][dy + dyDiff[directionIdx]]){
                                 distances[dx + dxDiff[directionIdx]][dy + dyDiff[directionIdx]] = distances[dx][dy] + rubbleLevels[dx][dy];
-                                predecessor[dx][dy] = directionIdx;
+                                predecessor[dx + dxDiff[directionIdx]][dy + dyDiff[directionIdx]] = directionIdx;
                             }
                         }
                     }
                 }
             }
         }
-        // Direction optDirection = MapLocation.directionTo(destination);
-        // int optEndX, optEndY;
-        // switch(optDirection){
-        //     case NORTH:
-        //         optEndX = 2;
-        //         optEndY = 4;
-        //         break;
-        //     case EAST:
-        //         optEndX = 4;
-        //         optEndY = 2;
-        //     case SOUTH:
-        //         optEndX = 2;
-        //         optEndY = 0;
-        //     case WEST:
-        //         optEndX = 0;
-        //         optEndY = 2;
-        //     case NORTHWEST:
-        //         optEndX = 4;
-        //         optEndY = 4;
-        //     case SOUTHWEST:
-        //         optEndX = 4;
-        //         optEndY = 0;
-        //     case SOUTHEAST:
-        //         optEndX = 0;
-        //         optEndY = 0;
-        //     default:
-        //         optEndX = 0;
-        //         optEndY = 4;
-        // }
-        // while (optEndX != 0 && optEndY != 0){
+    }
 
-        // }
+    // NOTE: going to assume that rubble is roughly additive, i.e. that the cost of the path you 
+    // take really only depends on the sum of the rubble you went through
+    public Direction returnBestDirection(MapLocation destination){
+        //if we can't even move, we can't do anything, so just... return lol
+        if (RobotController.getMovementCooldownTurns() > 0){
+            return CENTER;
+        }
+        //otherwise, do bellman ford to populate the arrays
+
+        this.bellmanFord(destination);
+        //note that for squares bigger than 5x5, this should probably be modified to comparing angles instead of cheaply doing this
+        //Need to somehow figure out the intermediate destination, returning CENTER for now
+        return CENTER;
     }
 
 }
