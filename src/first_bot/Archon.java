249908c748f2846067ec960f805f920d61ff4ded
edwardw21 <kingofgeedorah@gmail.com>
2022-01-05 22:08:02 -0500

    Update Archon.java
    archon relocation if rubble decreases by a ton

--- src/first_bot/Archon.java
+++ src/first_bot/Archon.java
@@ -9,6 +9,8 @@ public class Archon extends RobotCommon{
     // static RobotController rc;
 
     static int rank = -1; // 0-based
+    static boolean checkedNearby = false;
+    static MapLocation home;
 
     public Archon(RobotController rc){
         super(rc);
@@ -25,8 +27,51 @@ public class Archon extends RobotCommon{
         }
     }
 
+    //see if any nearby squares have significantly less rubble
+    public void relocCheck() throws GameActionException {
+        MapLocation loc = rc.getLocation();
+        int lx = loc.x;
+        int ly = loc.y;
+        int bestd = 0;
+        int bestr = rc.senseRubble(loc);
+        MapLocation newhome = loc;
+        for(int dx = 2; dx >= -2; dx--){
+            for(int dy = 2; dy >= -2; dy--){
+                int d = Util.max(Util.abs(dx), Util.abs(dy));
+                if(d == 0){
+                    continue;
+                }
+                MapLocation mc = new MapLocation(dx - 2 + lx, dy - 2 + ly);
+                try{//see if mc is a viable place to move to
+                    int rub = rc.senseRubble(mc);
+                    if((10 + rub) * (100 - 10 * bestd) < (10 + bestr) * (100 - 10 * d)){
+                        bestd = d;
+                        bestr = rub;
+                        newhome = mc;
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        home = newhome;
+    }
+
     public void takeTurn() throws GameActionException {
         if(rank == -1) establishRank();
+
+        if(!checkedNearby){
+            relocCheck();
+            checkedNearby = true;
+        }
+
+        if(me != home){//we should try moving to a nearby place with less rubble
+            Direction dir = me.directionTo(home);
+            if(rc.canMove(dir)){
+                rc.move(dir);
+                me = rc.getLocation();
+            }
+        }
 
         rc.setIndicatorString(Integer.toString(rank));
 
