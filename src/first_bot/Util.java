ecf4e3e773c267c934a79481b8f9a994f06afa8d
Matthew Ho <homattieho@gmail.com>
2022-01-07 01:19:41 -0800

    Modified how archon location is stored in memory, so that if the number is 0 we can tell there was no archon present.

--- src/first_bot/Util.java
+++ src/first_bot/Util.java
@@ -71,11 +71,11 @@ public class Util {
     }
 
     public static MapLocation getLocationFromInt(int loc) {
-        return new MapLocation(loc / 64, loc % 64);
+        return new MapLocation(loc - 1 / 64, loc - 1 % 64);
     }
 
     public static int getIntFromLocation(MapLocation loc) {
-        return loc.x * 64 + loc.y;
+        return loc.x * 64 + loc.y + 1;
     }
 
     public static int abs(int a){
