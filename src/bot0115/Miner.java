2be324dd81390bb9cf86e8c9675382fa1fa38af8
cosinechicken <ah.blenden@gmail.com>
2022-01-16 11:01:45 -0800

    Created bot0115 from my previous commit

---
+++ src/bot0115/Miner.java
@@ -0,0 +1,297 @@
+package bot0115;
+
+import java.util.Map;
+
+import battlecode.common.*;
+
+
+public class Miner extends Unit{
+    public static boolean reachedTarget;
+    public static RobotInfo[] robotLocations;
+    static RobotInfo[] nearbyBotsSeen;
+    static int numEnemies;
+    static MapLocation enemySoldierCentroid = new MapLocation(0, 0);
+    static int maxBytecodeUsed = 0;
+
+    public Miner(RobotController rc, int r, MapLocation loc, MapLocation t) {
+        super(rc, r, loc);
+        isRetreating = false;
+        target = t;
+    }
+    
+    public void takeTurn() throws GameActionException {
+        me = rc.getLocation();
+        round = rc.getRoundNum();
+        archonLocation = nearestArchon(me);
+        rc.setIndicatorString("MINER: " + me + " " + archonLocation + " " + target + " " + reachedTarget);
+        robotLocations = rc.senseNearbyRobots();
+        if (me.isAdjacentTo(target) && rc.senseRubble(target) > 30){
+            target = target.translate(rng.nextInt(Util.WIDTH) - target.x, rng.nextInt(Util.HEIGHT) - target.y);
+        }
+        observe();
+        observeSymmetry();
+        tryToMine(1);
+        nearbyBotsSeen = rc.senseNearbyRobots(visionRadius);
+        numEnemies = 0;
+        double enemySoldierCentroidx = 0;
+        double enemySoldierCentroidy = 0;
+        for (RobotInfo bot : nearbyBotsSeen){
+            switch(bot.getType()){
+                case SOLDIER:
+                    if (bot.getTeam() != myTeam){
+                        MapLocation enemyLoc = bot.getLocation();
+                        numEnemies++;
+                        enemySoldierCentroidy += enemyLoc.y;
+                        enemySoldierCentroidx += enemyLoc.x;
+                    }
+                    break;
+                default:
+            }
+        }
+        enemySoldierCentroidx /= numEnemies;
+        enemySoldierCentroidy /= numEnemies;
+        enemySoldierCentroid = enemySoldierCentroid.translate((int) enemySoldierCentroidx - enemySoldierCentroid.x, (int) enemySoldierCentroidy - enemySoldierCentroid.y);
+        
+        /*
+        // If there are mineable neighboring deposits, don't keep moving
+        if (!isRetreating) {
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                    if (rc.canSenseLocation(mineLocation) && rc.senseGold(mineLocation) > 0) {
+                        rc.setIndicatorString("SENSED GOLD");
+                        return;
+                    }
+                    if (rc.canSenseLocation(mineLocation) && rc.senseLead(mineLocation) > 1) {
+                        rc.setIndicatorString("SENSED LEAD");
+                        return;
+                    }
+                }
+            }
+        }
+        */
+        // System.out.println(round + ": " + rc.getID() + ", " + Clock.getBytecodeNum());
+        if (rc.canSenseLocation(target)) {
+            RobotInfo[] robotAtTarget = rc.senseNearbyRobots(target, 2, rc.getTeam());
+            for (RobotInfo robot : robotAtTarget) {
+                if (robot.getType().equals(RobotType.MINER) && robot.getID() < rc.getID()) {
+                    tryToWriteTarget(true);
+                    break;
+                }
+            }
+        }
+
+        // Case when Archon could not assign a Location to the Miner
+        if(target.equals(archonLocation) && isRetreating == false) {
+            // explore();
+            // System.out.println(rc.getID() + ": finding new location");
+            tryToWriteTarget(true);
+        }
+        
+        if(!reachedTarget && me.equals(target)) {
+            // somehow stay still if lots of lead
+            reachedTarget = true;
+            tryToWriteTarget(true);
+        }
+        tryToMove();
+        tryToWriteTarget(false);
+        tryToMine(1);
+    }
+
+    // When the Archon has no valid targets for Miner, it should explore until it reaches a far away lead location.
+    //This function should be replaced with a better exploration algorithm once we think of one
+    public void explore() throws GameActionException {
+        // stay put if you're on lattice and you can mine
+        if(Util.onLattice(Util.getIntFromLocation(me))) {
+            MapLocation loc = me;
+            if (rc.senseNearbyLocationsWithLead(2).length > 0) {
+                target = me;
+                return;
+            }
+        }
+        // otherwise, explore with higher chance of moving away from Archon
+        int dirIndex = rng.nextInt(Util.directions.length + 4);
+        Direction dir = Direction.CENTER;
+        if(dirIndex < Util.directions.length) {
+            dir = Util.directions[dirIndex];
+        }
+        else {
+            dir = me.directionTo(archonLocation).opposite();
+        }
+        
+        Direction dirLeft = dir.rotateLeft();
+        Direction dirRight = dir.rotateRight();
+
+        if(rc.canMove(dir)) {
+            rc.move(dir);
+        } else if (rc.canMove(dirLeft)) {
+            rc.move(dirLeft);
+        } else if (rc.canMove(dirRight)) {
+            rc.move(dirRight);
+        }
+        me = rc.getLocation();
+    }
+
+    // When exploring, the Miner should write the furthest gold/lead location it can see to shared array.
+    // Returns if target was written
+    // resetLoc is true if we want a new location even if we haven't reached the old one yet
+    public boolean tryToWriteTarget(boolean resetLoc) throws GameActionException {
+        
+        MapLocation[] goldLocations = rc.senseNearbyLocationsWithGold(getVisionRadiusSquared());
+        int numGoldLocations = goldLocations.length;
+        boolean change = false;
+        robotLocations = rc.senseNearbyRobots();
+        if(numGoldLocations > 0) {
+            MapLocation bestLoc = Util.getLocationFromInt(rc.readSharedArray(Util.getArchonMemoryBlock(rank) + 1));
+            int bestDist = bestLoc.distanceSquaredTo(archonLocation);
+
+            for(int i = 0; i < numGoldLocations; i++) {
+                MapLocation newLoc = goldLocations[i];
+                boolean occupied = false;
+                RobotInfo[] robotAtTarget = rc.senseNearbyRobots(target, 2, rc.getTeam());
+                for (RobotInfo robot : robotAtTarget) {
+                    if (robot.getTeam().equals(rc.getTeam()) && robot.getType().equals(RobotType.MINER) && robot.getID() < rc.getID() && robot.getLocation().distanceSquaredTo(target) <= 2) {
+                        occupied = true;
+                        break;
+                    }
+                }
+                if (occupied) {
+                    continue;
+                }
+                int newDist = archonLocation.distanceSquaredTo(newLoc);
+                if(newDist > bestDist) {
+                    bestDist = newDist;
+                    bestLoc = newLoc;
+                    change = true;
+                }
+            }
+
+            if(change) {
+                target = bestLoc;
+                if (target.equals(me) == false) {
+                    reachedTarget = false;
+                }
+                // rc.writeSharedArray(Util.getArchonMemoryBlock(rank) + 1, Util.moveOnLattice(Util.getIntFromLocation(bestLoc)));
+                // System.out.println("Miner " + rc.getID() + " to (" + target.x + ", " + target.y + "), gold, turn " + round);
+                return true;
+            }
+        }
+
+        MapLocation[] leadLocations = rc.senseNearbyLocationsWithLead(getVisionRadiusSquared());
+        
+        int numLeadLocations = leadLocations.length;
+        int myId = rc.getID();
+        if(numLeadLocations > 0) {
+            // int initialBytecode = 0;
+            // if (rc.getID() == 10080){
+            //     initialBytecode = Clock.getBytecodeNum();
+            // }
+            MapLocation bestLoc = archonLocation;
+            int bestDist = 0;
+            int[][] gridOfSquares = new int[9][9];
+            for(int idx = numLeadLocations - 1; idx >= 0 ; idx--) {
+                if (rc.senseLead(leadLocations[idx]) > 1){
+                    gridOfSquares[leadLocations[idx].x - me.x + 4][leadLocations[idx].y - me.y + 4]++;
+                }
+            }
+            for (int idx = robotLocations.length - 1; idx >= 0; idx--){
+                int robotX = robotLocations[idx].location.x - me.x + 4;
+                int robotY = robotLocations[idx].location.y - me.y + 4;
+                if (robotLocations[idx].getTeam().equals(rc.getTeam()) && robotLocations[idx].getType().equals(RobotType.MINER) && robotLocations[idx].getID() < myId){
+                    for (int dxdyIdx = 7; dxdyIdx >= 0; dxdyIdx--){
+                        int xcoord = Util.dxDiff[dxdyIdx] + robotX;
+                        int ycoord = Util.dyDiff[dxdyIdx] + robotY;
+                        if (xcoord >= 0 && xcoord <= 8 && ycoord >= 0 && ycoord <= 8){
+                            gridOfSquares[xcoord][ycoord] = 0;
+                        }
+                    }
+                    gridOfSquares[robotX][robotY] = 0;
+                }
+            }
+            for(int idx = numLeadLocations - 1; idx >= 0 ; idx--) {
+                if (gridOfSquares[leadLocations[idx].x - me.x + 4][leadLocations[idx].y - me.y + 4] > 0 && bestDist < archonLocation.distanceSquaredTo(leadLocations[idx])){
+                    bestDist = archonLocation.distanceSquaredTo(leadLocations[idx]);
+                    bestLoc = leadLocations[idx];
+                    change = true;
+                }
+            }
+            // for(int i = 0; i < numLeadLocations; i++) {
+            //     MapLocation newLoc = leadLocations[i];
+            //     boolean occupied = false;
+            //     for (RobotInfo robot : robotLocations) {
+            //         if (robot.getTeam().equals(rc.getTeam()) && robot.getType().equals(RobotType.MINER) && robot.getID() < rc.getID() && robot.getLocation().distanceSquaredTo(target) <= 2) {
+            //             occupied = true;
+            //             break;
+            //         }
+            //     }
+            //     if (occupied) {
+            //         continue;
+            //     }
+            //     int newDist = archonLocation.distanceSquaredTo(newLoc);
+            //     int numLead = rc.senseLead(newLoc);
+            //     if(numLead > 1 && newDist > bestDist) {
+            //         bestDist = newDist;
+            //         bestLoc = newLoc;
+            //         change = true;
+            //         break;
+            //     }
+            // }
+            if(change) {
+                target = bestLoc;
+                if (target.equals(me) == false) {
+                    reachedTarget = false;
+                }
+                // rc.writeSharedArray(Util.getArchonMemoryBlock(rank) + 1, Util.moveOnLattice(Util.getIntFromLocation(bestLoc)));
+                // System.out.println("Miner " + rc.getID() + " to (" + target.x + ", " + target.y + "), lead, turn " + round);
+                return true;
+            }
+        }
+        // Choose random location
+        if (resetLoc || reachedTarget) {
+            MapLocation bestLoc = new MapLocation(rng.nextInt(Util.WIDTH), rng.nextInt(Util.HEIGHT));
+            while (bestLoc.distanceSquaredTo(archonLocation) <= 10) {
+                bestLoc = new MapLocation(rng.nextInt(Util.WIDTH), rng.nextInt(Util.HEIGHT));
+            }
+            target = bestLoc;
+            
+            if (target.equals(me) == false) {
+                reachedTarget = false;
+            }
+            // System.out.println("Miner " + rc.getID() + " to (" + target.x + ", " + target.y + "), random, turn " + round);
+            // rc.writeSharedArray(Util.getArchonMemoryBlock(rank) + 1, Util.moveOnLattice(Util.getIntFromLocation(bestLoc)));
+        }
+        return false;
+    }
+
+    // Tries to mine in 3x3 square around Miner, and leaves leaveLead amount at location
+    public void tryToMine(int leaveLead) throws GameActionException {
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                while (rc.canMineGold(mineLocation) && rc.senseGold(mineLocation) > 0) {
+                    rc.mineGold(mineLocation);
+                }
+                while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > leaveLead) {
+                    rc.mineLead(mineLocation);
+                }
+            }
+        }
+    }
+
+    // Moves toward target through pathfinding
+    public void tryToMove() throws GameActionException {
+        Direction dir = Direction.CENTER;
+        if (!isRetreating) {
+            dir = pf.findBestDirection(target, 80);
+        } else {
+            dir = pf.findBestDirection(target, 10);
+        }
+        
+        if (rc.canMove(dir)) {
+            rc.setIndicatorLine(me, me.translate(dir.dx, dir.dy), 0, 100, 0);
+            rc.move(dir);
+            
+            me = rc.getLocation();
+        }
+    }
+}
---
+++ src/bot0115/MinerLattice.java
@@ -0,0 +1,84 @@
+package bot0115;
+
+import battlecode.common.*;
+
+public class MinerLattice extends Miner {
+    public MinerLattice(RobotController rc, int r, MapLocation loc, MapLocation t) throws GameActionException {
+        super(rc, r, loc, t);
+        System.out.println("MINER LATTICE: " + me + " " + archonLocation + " " + target + " " + reachedTarget);
+    }
+
+    public void takeTurn() throws GameActionException {
+        me = rc.getLocation();
+        rc.setIndicatorString("MINER LATTICE: " + me + " " + archonLocation + " " + target + " " + reachedTarget);
+
+        /*
+            general movement behavior:
+                - if you see an open spot on lattice and you are not on the lattice, move towards it and stay
+                - else if you see a spot with no lead, move towards it and disintegrate (prioritize on lattice spots)
+                - else (you have no target or your target is occupied by a miner),
+                    test a lattice point that is away from original archon location
+                    if that's new go for it
+                    otherwise disintegrate oops (maybe better behavior later)
+
+            every 20 turns, if you are on the lattice, update target to be the lattice point that is away from 
+                your original archon location
+                - if your new target currently has a miner stay put
+                - ^ this behavior will cause the movement to go in waves (i predict), best i can do for now
+                - if your new target is out of bounds, check if there are any open lattice locations around
+            
+            report back empty locations to archon
+                - closest locations win
+        */
+
+        MapLocation newTarget = findNewTarget();
+
+        tryToMine(1);
+    }
+
+    // Follows the logic flowchart for assigning a new target
+    public MapLocation findNewTarget() throws GameActionException {
+        /*int bestDist = me.distanceSquaredTo(target);
+        int bestValue = 3; // 3 - original target, 2 - no lead spot, 1 - no lead spot on lattice, 0 - lead spot on lattice
+        MapLocation bestTarget = target;
+        MapLocation[] visible = rc.getAllLocationsWithinRadiusSquared(me, bestDist - 1);
+            // function automatically uses minimum of vision radius and distance, and we only care about closer locations
+        for(MapLocation loc : visible) {
+            if(rc.isLocationOccupied()) continue;
+            int curDist = me.distanceSquaredTo(loc);
+            int curValue = 3;
+            if(rc.senseLead(loc) == 0) curValue = 2;
+            
+            switch(bestCase) {
+                case 4:
+                case 3:
+                    if(rc.senseLead(loc) == 0 && curDist < bestDist && (bestCase > 3 || (bestCase == 3 && curDist < bestDist))) {
+                        bestDist = curDist;
+                        bestTarget = loc;
+                        bestCase = 3;
+                    }
+                case 2:
+                    if(rc.senseLead(loc) == 0 && Util.onLattice(loc) && (bestCase > 2 || (bestCase == 2 && curDist < bestDist))) {
+                        bestDist = curDist;
+                        bestTarget = loc;
+                        bestCase = 2;
+                    }
+                case 1:
+                    if(rc.senseLead(loc) > 0 && Util.onLattice(loc) && (bestCase > 1 || (bestCase == 1 && curDist < bestDist))) {
+                        bestDist = curDist;
+                        bestTarget = loc;
+                        bestCase = 1;
+                    }
+                default:
+                    break;
+            }
+        }*/
+        return target;
+    }
+
+    // Takes in a current location on lattice and returns new one opposite from the Archon
+    public MapLocation findNewOnLattice(MapLocation cur) throws GameActionException{
+        Direction dir = archonLocation.directionTo(cur);
+        return Util.moveOnLattice(cur.translate(3 * dir.getDeltaX(), 3 * dir.getDeltaY())); // moveOnLattice for edge cases
+    }
+}
